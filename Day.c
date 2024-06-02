82c4c91a6ac94f2498b5b530d03f4766661f5230
Motunrayo Adeneye <motunrayodeneye@gmail.comgit config --global user.name Motunrayo>
2024-06-02 21:33:59 +0100

    30daysofC_1
    Write a C program that takes an integer n as input from the user,

--- Day.c
+++
@@ -1,5 +0,0 @@
-/*
-Write a C program that takes an integer n as input from the user, 
-and then takes n integers as input from the user. 
-The program should calculate the average of these n integers 
-and print the result to the console.*/
