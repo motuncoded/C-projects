0dfc3c07fe312fe2ec015365e6f4efd6f2774073
Motunrayo Adeneye <motunrayodeneye@gmail.comgit config --global user.name Motunrayo>
2024-06-02 21:52:35 +0100

    30daysofC_2
    A program to check if is prime or not

--- Day2.c
+++ Day2.c
@@ -1,9 +1,37 @@
 /*
-Write a C program that takes an integer as input from the user and checks if the number is prime. If the number is prime, the program should print "The number is prime." Otherwise, it should print "The number is not prime.
-  input/ouput
+Write a C program that takes an integer as input from the user 
+and checks if the number is prime. If the number is prime, 
+the program should print "The number is prime."
+ Otherwise, it should print "The number is not prime.
 
-  Enter a number: 7
-The number is prime.
 
 
 "*/
+
+#include <stdio.h>
+
+
+
+int is_prime(int num){
+  if(num <= 1){
+    return 0;
+
+  }
+  for (int i = 2; i * i <= num; i++){
+    if(num % i == 0){
+      return 0;
+    }
+  }
+  return 1;
+}
+int main(){
+  int num;
+  printf("Enter an integer: ");
+  scanf("%d", &num);
+  if (is_prime(num)) {
+        printf("The number is prime. \n");
+      }else(
+        printf("The number is not prime. \n")
+        );
+
+} // to run =>  gcc Day2.c -o Day2 && Day2.exe
