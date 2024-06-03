a101bfba781bebe2dfa5bb17449394bc30a093c2
Motunrayo Adeneye <67984447+motuncoded@users.noreply.github.com>
2024-06-03 20:45:23 +0100

    30daysof c 2 (#31)
    * day30&31
    
    I learnt about the use of c pointer to access array values and also change it
    
    * day32
    
    I learnt about use of C pointer and function together
    
    * 30daysofC_1
    
    Write a C program that takes an integer n as input from the user,
    
    * 30daysofC_2
    
    A program to check if is prime or not
    
    ---------
    
    Co-authored-by: Motunrayo Adeneye <motunrayodeneye@gmail.comgit config --global user.name Motunrayo>

--- Day2.c
+++ Day2.c
@@ -2,6 +2,13 @@
 Write a C program that takes an integer as input from the user 
 and checks if the number is prime. If the number is prime, 
 the program should print "The number is prime."
+ Otherwise, it should print "The number is not prime."
+ */
+
+#include <stdio.h>
+
+
+
  Otherwise, it should print "The number is not prime.
 
 
