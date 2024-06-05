fe610a5e687ce425fdfcaddbe968efeff0949743
Motunrayo Adeneye <motunrayodeneye@gmail.comgit config --global user.name Motunrayo>
2024-06-05 17:09:27 +0100

    day35
    In this change, I learnt about file Handling

---
+++ Project_19.c
@@ -0,0 +1,15 @@
+
+/*
+Create a program that dynamically changes the size of an array.
+
+Create a pointer array like this
+int* ages;
+Allocate 4 spaces for the array using the malloc() function
+Get 4 input values for the array.
+Print the array elements using the pointer.
+Resize the array to store 6 elements using realloc()
+Assign 32 and 59 as the 5th and 6th elements.
+Hint
+ages[4] = 32;
+ages[5] = 49;
+Then finally Print all 6 array elements.*/
