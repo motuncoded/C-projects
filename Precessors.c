47cb48800ec266cf33d24bcbe282ca149a1a432a
Motunrayo Adeneye <67984447+motuncoded@users.noreply.github.com>
2024-06-07 20:27:40 +0100

    Day34 (#56)
    * day30&31
    
    I learnt about the use of c pointer to access array values and also change it
    
    * day32
    
    I learnt about use of C pointer and function together
    
    * 30daysofC_1
    
    Write a C program that takes an integer n as input from the user,
    
    * 30daysofC_2
    
    A program to check if is prime or not
    
    * Day33
    
    I learnt about the use of struct that serves to store data like objects
    
    * day34
    
    In this file, I learnt abooit dynamic memory allocation  closes #33
    
    * day35
    
    In this change, I learnt about file Handling
    
    * day36
    
    preprocessors and marcos
    
    ---------
    
    Signed-off-by: Motunrayo Adeneye <motunrayodeneye@gmail.comgit config --global user.name Motunrayo>
    Co-authored-by: Motunrayo Adeneye <motunrayodeneye@gmail.comgit config --global user.name Motunrayo>

---
+++ Precessors.c
@@ -0,0 +1,25 @@
+/*
+In C, a preprocessor is a program that processes the 
+source code before it is compiled. 
+The preprocessor reads the source code, 
+performs certain operations on it, 
+and then passes the modified code to the compiler.
+examples of preprocessor
+#include, #define, #undef, #ifdef, #ifnded etc
+*/
+
+/*
+for example if we use #include <stdio.h>, we will  be able to use the printf() and scanf()
+
+Create a program to find the square root
+of a number using the sqrt() function and
+macro together.
+
+ Include the math.h header file.
+ Define a macro function squareRoot(n).
+ Inside the main function, use the 
+  squareRoot() macro to compute the 
+  square root
+
+
+*/
